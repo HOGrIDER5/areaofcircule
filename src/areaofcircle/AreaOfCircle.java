a2852e2ced0ff80c6500ee17ba2b104eb54314e3
amsta5248 <amsta5248@7HGM6Y1.Student.UGDSB.ED>
2018-10-09 09:27:57 -0400

    java

---
+++ src/areaofcircle/AreaOfCircle.java
@@ -0,0 +1,34 @@
+/*
+ * Amer Stas.
+ * 18/09/2018
+ * Calcualte AreaOfCircle.
+ */
+
+package areaofcircle;
+
+/**
+ *
+ * @author amsta5248
+ */
+public class AreaOfCircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double radius = 15.0;
+       double pi = 3.14;
+       double area = 0.0 ;
+       // algorthim for are calc
+       
+       area= pi* radius* radius ;
+       System.out.println ("the area ..."+ area+ "cm squared");
+    
+               
+               
+             
+   
+        // TODO code application logic here
+    }
+    
+}
